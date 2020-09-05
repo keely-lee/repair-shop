e661c298262983d44b9b5d119136302d51075873
keely_lee <keely_lee@outlook.com>
2020-09-05 00:09:56 -0400

    Add svg img

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const ctx = document.getElementById("canvas-home");
-  const context = ctx.getContext('2d');
+  // const ctx = document.getElementById("canvas-home");
+  // const context = ctx.getContext('2d');
 
-  ctx.width = document.documentElement.clientWidth;
-  ctx.height = document.documentElement.clientHeight;
-  const person1 = new Image();
-  person1.src = "src/images/person1.png";
-  person1.onload = function(){
-    context.drawImage(person1, 100, 100, person1.width/13, person1.height/13)
-  }
+  // ctx.width = document.documentElement.clientWidth;
+  // ctx.height = document.documentElement.clientHeight;
+  // const person1 = new Image();
+  // person1.src = "src/images/person1.png";
+  // person1.onload = function(){
+  //   context.drawImage(person1, 100, 100, person1.width/13, person1.height/13)
+  // }
 
-  ctx.setAttribute("class", "test")
-  person1.addEventListener('click', repair)
+  // ctx.setAttribute("class", "test")
+  // person1.addEventListener('click', repair)
 
 
 
-  function repair(){
-    //clear canvas
-    //draw first obj
-    console.log("CLICKITY CLACK")
-    ctx.style.backgroundImage = "none";
-  }
+  // function repair(){
+  //   //clear canvas
+  //   //draw first obj
+  //   console.log("CLICKITY CLACK")
+  //   ctx.style.backgroundImage = "none";
+  // }
 
 })
