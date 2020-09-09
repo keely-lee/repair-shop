800dcee4da90ed5708bf24efbbf80f5bd1d1beef
keely_lee <keely_lee@outlook.com>
2020-09-09 17:23:42 -0400

    Remove unwanted dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// import sketchFunc from './my-plugin/sketch-app'
-import mycommand from './my-plugin/src/my-command';
 
 document.addEventListener("DOMContentLoaded", () => {
   const per1 = document.getElementsByClassName("person1")[0];
