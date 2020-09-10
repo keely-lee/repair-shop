6d293dbeb02300898a09aece0bb37fcf7e25df3f
keely_lee <keely_lee@outlook.com>
2020-09-10 13:08:44 -0400

    Test event listeners

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const per1 = document.getElementsByClassName("person1")[0];
   per1.addEventListener("click", repair)
 
-
-
   function repair(){
     console.log("CLICKITY CLACK")
-
-    document.getElementById("content").innerHTML =
-      '<object type="text/html" data="paint.html" ></object>';
-
+    document.getElementById("paint").innerHTML = '<object type="text/html" data="paint.html" ></object>';
   }
 
 })
