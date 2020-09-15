df582f5160592ebd0718d0ca9df817b9f68ace35
keely_lee <keely_lee@outlook.com>
2020-09-15 15:34:29 -0400

    Add home event listener

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+  const home = document.getElementById("home-button");
+  home.addEventListener("click", () => {
+    document.getElementById("paint").innerHTML = "";
+  })
+
   const per1 = document.getElementsByClassName("person1")[0];
-  console.log(per1)
-  console.log("PER 1")
   per1.addEventListener("click", repair)
 
   function repair(){
     console.log("CLICKITY CLACK")
     document.getElementById("paint").innerHTML = '<object id="paint-obj" type="text/html" data="paint.html" ></object>';
+
   }
-
-
 
 })
